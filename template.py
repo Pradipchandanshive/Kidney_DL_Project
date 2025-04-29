b74711181ffd55aab9c45809eef79096108fc79f
Prdipchandanshive <chandanshivepradip48@gmail.com>
2025-04-29 18:07:32 +0530

    set up done

--- template.py
+++ template.py
@@ -6,7 +6,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = 'cnnClassifier'
-
+    
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
