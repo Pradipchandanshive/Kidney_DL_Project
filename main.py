4103a55e4eaa91ff016d97246f00b34e104b3acd
Prdipchandanshive <chandanshivepradip48@gmail.com>
2025-05-06 18:09:02 +0530

    prepare_base_model

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from src.cnnClassifier.entity.config import DataIngestionConfig
 from src.cnnClassifier.config.configuration import ConfigurationManager 
 from src.cnnClassifier.components.Data_ingestion_01 import DataIngestion
 from src.cnnClassifier.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -17,4 +18,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
    logger.exception(e) 
-   raise e  
+   raise e 
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
